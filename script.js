ccbbcc5cae6193d3b67e0a0be4b8af2a35c27f0b
Adolfo Herrera <valherdolf@gmail.com>
2021-06-25 05:03:09 -0400

    Removed the data attribute to every element in the DOM, and added a class instead, added an eventListener to all the numbers so when clicked now you can see the number in the display.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let displayValue = [];
+console.log(displayValue)
+const currentValue = document.querySelector(".js-currentValue");
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, (e) => {
+        if (e.target.matches(selector)) callback(e);
+    })
+}
+
+addGlobalEventListener("click", ".js-number", function(e) {
+    displayValue.push(e.target.textContent)
+    currentValue.textContent = displayValue.join("");
+    console.log(displayValue)
+    }
+)
+
+/* --- operator functions --- */
 function add(...operand) {
     console.log(operand.reduce((total, currentValue) => total + currentValue))
 }
@@ -20,12 +38,3 @@ function operate(operator, ...operand) {
     if(operator === "multiply") { return multiply(...operand) }
     if(operator === "divide") { return divide(...operand) }
 }
-add(1, 1)
-substract(15, 5)
-multiply(5, 5)
-divide(10, 2)
-
-operate("divide", 40, 4)
-
-const datas = document.querySelectorAll("[data-number]"); /* Should I use Data or class to find an element */
-console.log(datas)
