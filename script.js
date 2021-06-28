47fa7a495ba4f7252cba8370f3d280b19c547537
Adolfo Herrera <valherdolf@gmail.com>
2021-06-28 05:22:18 -0400

    Added a guard clause to prevent adding two operators sequenly, also and if statement for when you press equal, it will calculate de result. Still needs more work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let displayValue = [];
+let operationToOperate = [];
 console.log(displayValue)
 const currentValue = document.querySelector(".js-currentValue");
 
@@ -9,27 +10,84 @@ function addGlobalEventListener(type, selector, callback) {
 }
 
 addGlobalEventListener("click", ".js-number", function(e) {
-    displayValue.push(e.target.textContent)
+    displayValue.push(e.target.textContent);
+    operationToOperate.push(e.target.textContent);
     currentValue.textContent = displayValue.join("");
-    console.log(displayValue)
+    console.log(displayValue);
     }
 )
 
+addGlobalEventListener("click", ".js-allClear", function() {
+    displayValue = [];
+    console.log(displayValue);
+    currentValue.textContent = "";
+    }
+)
+
+addGlobalEventListener("click", ".js-operator", function(e) {
+    if(currentValue.textContent.includes(e.target.textContent, displayValue.length - 1)) {return;}
+
+    if(e.target.name === "equals") {
+        console.log(operationToOperate)
+        const isOperator = operationToOperate.findIndex(operator => isNaN(operator))
+        let left = [];
+        let right = [];
+        for(let i = 0; i < isOperator; i++) {
+            left.push(parseInt(operationToOperate[i]))
+        }
+        for(let i = isOperator; i < operationToOperate.length; i++) {
+            if (isNaN(operationToOperate[i])) continue;
+            right.push(parseInt(operationToOperate[i]))
+        }
+        
+        console.log(left.join(""))
+        console.log(right)
+        let result = operate(operationToOperate[isOperator], Number (left.join("")), Number (right.join("")))
+        currentValue.textContent = result;
+        return console.log(result)
+    }
+    // When you press equal, then check all the numbers before the operator, and all the numbers after the operator
+    // will use findIndex to start from that value to now create a new array for left and a new one for right
+
+    console.log(e.target.name)
+    displayValue.push(e.target.textContent);
+    operationToOperate.push(e.target.name);
+    currentValue.textContent = displayValue.join("");
+
+    }
+)
+
+// addGlobalEventListener("click", ".js-operator", function(e) {
+//     if((displayValue.includes(e.target.textContent, displayValue.length + 1))) { 
+//         return;
+//     }
+//     else {
+//     console.log(e.target.name)
+//     displayValue.push(e.target.textContent);
+//     currentValue.textContent = displayValue.join("");
+//     }
+//     }
+// )
+
 /* --- operator functions --- */
 function add(...operand) {
-    console.log(operand.reduce((total, currentValue) => total + currentValue))
+    //console.log(operand.reduce((total, currentValue) => total += currentValue))
+    return operand.reduce((total, currentValue) => total + currentValue)
 }
 
 function substract(...operand) {
-    console.log(operand.reduce((total, currentValue) => total - currentValue))
+    //console.log(operand.reduce((total, currentValue) => total - currentValue))
+    return operand.reduce((total, currentValue) => total - currentValue)
 }
 
 function multiply(...operand) {
-    console.log(operand.reduce((total, currentValue) => total * currentValue))
+    //console.log(operand.reduce((total, currentValue) => total * currentValue))
+    return operand.reduce((total, currentValue) => total * currentValue)
 }
 
 function divide(...operand) {
-    console.log(operand.reduce((total, currentValue) => total / currentValue))
+    //console.log(operand.reduce((total, currentValue) => total / currentValue))
+    return operand.reduce((total, currentValue) => total / currentValue)
 }
 
 function operate(operator, ...operand) {
